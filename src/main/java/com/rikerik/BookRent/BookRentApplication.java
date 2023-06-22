6cca68c5adacf6d02921925665d5aaec54ed0921
rikerik <bako.erik.dominik@gmail.com>
2023-06-22 15:40:57 +0200

    db connection and started Crud

--- src/main/java/com/rikerik/BookRent/BookRentApplication.java
+++ src/main/java/com/rikerik/BookRent/BookRentApplication.java
@@ -1,13 +1,48 @@
 package com.rikerik.BookRent;
 
+import com.rikerik.BookRent.DAO.UserRepository;
+import com.rikerik.BookRent.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("bookrent")
+
 public class BookRentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookRentApplication.class, args);
-	}
+    private final UserRepository userRepository;
+
+    public BookRentApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookRentApplication.class, args);
+
+    }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK); //return all users
+    }
+
+    @PostMapping("/CreateUser")
+    public User CreateUser(User user) {
+        return user; //TODO
+    }
+
 
 }
---
+++ src/main/java/com/rikerik/BookRent/Model/User.java
@@ -0,0 +1,36 @@
+package com.rikerik.BookRent.Model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@Entity // to map object to db
+@Table(name = "Users") //name the table
+@Data //generates all boilerplate code, like getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder //generating a builder pattern for the class
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "ID_SEQ", // name of seq. gen.
+            sequenceName = "ID_SEQ", //db seq. name for generating pk values
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE, //generating by sequence
+            generator = "ID_SEQ" //the name of the generator
+    )
+    @Column(name = "id")
+    private long userId;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "email", nullable = false)
+    private String email;
+
+
+}
