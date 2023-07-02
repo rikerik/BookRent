5722d498dd88532d6236b136f7fdd19e66ddccee
rikerik <bako.erik.dominik@gmail.com>
2023-07-02 08:56:23 +0200

    Book class added

---
+++ src/main/java/com/rikerik/BookWave/Model/Book.java
@@ -0,0 +1,37 @@
+package com.rikerik.BookWave.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    @Id
+    @SequenceGenerator(name = "BOOK_ID_SEQ",
+            sequenceName = "BOOK_ID_SEQ",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "BOOK_ID_SEQ"
+    )
+    @Column(name = "bookId", nullable = false, columnDefinition = "serial")
+    private long bookId;
+    @Column(name ="title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "publisher",nullable = false)
+    private String publisherName;
+    @Column(name = "available",nullable = false)
+    private boolean isRented;
+    @Lob
+    @Column (name = "image")
+    private byte[] image;
+}
